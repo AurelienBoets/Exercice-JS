b781296879f3c2bbdf54ff5b8374e8bdf683ea33
Jlnasa <aurelien.boets59@gmail.com>
2023-09-07 13:39:34 +0200

    commit

--- Exo13/script.js
+++ Exo13/script.js
@@ -2,15 +2,15 @@ let year = Number(prompt("age de l'enfant"));
 
 if (year < 3) {
   document.body.innerHTML = "Votre enfant est trop jeune";
-} else if (year > 2 && year < 7) {
+} else if (year < 7) {
   document.body.innerHTML = "Votre enfant est dans la catégorie Baby";
-} else if (year > 6 && year < 9) {
+} else if (year < 9) {
   document.body.innerHTML = "Votre enfant est dans la catégorie Poussin";
-} else if (year > 8 && year < 11) {
+} else if (year < 11) {
   document.body.innerHTML = "Votre enfant est dans la catégorie Pupille";
-} else if (year > 10 && year < 13) {
+} else if (year < 13) {
   document.body.innerHTML = "Votre enfant est dans la catégorie Minime";
-} else if (year > 12 && year < 18) {
+} else if (year < 18) {
   document.body.innerHTML = "Votre enfant est dans la catégorie Cadet";
 } else {
   document.body.innerHTML = "Ce n'est plus un enfant";
